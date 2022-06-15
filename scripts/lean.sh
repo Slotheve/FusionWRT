c62f2ab74ab5ca668667b96be2c59a36fce81196
Slotheve <85428225+Slotheve@users.noreply.github.com>
2022-06-16 00:05:08 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -86,23 +86,23 @@ svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
 
 # Mod zzz-default-settings
-pushd package/lean
-popd
-
-# Mod zzz-default-settings
-pushd package/lean/default-settings/files
+cd
+cd ~/actions-runner/_work/FusionWRT/FusionWRT/openwrt/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
 sed -i '/18.06/d' zzz-default-settings
 export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-popd
+cd
+cd ~/actions-runner/_work/FusionWRT/FusionWRT/openwrt/
 
 # Fix libssh
-pushd feeds/packages/libs
+cd
+cd ~/actions-runner/_work/FusionWRT/FusionWRT/openwrt/feeds/packages/libs
 rm -rf libssh
 svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd
+cd
+cd ~/actions-runner/_work/FusionWRT/FusionWRT/openwrt/
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
